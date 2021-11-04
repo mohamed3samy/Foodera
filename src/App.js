42d972610ef4001fa5303b685b41ab247159866a
mohamed3samy <mohamed701samy@gmail.com>
2021-11-04 09:57:48 +0200

    Foodera is a food order app created by React.js

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+	const [cartIsShow, setCartIsShow] = useState(false);
+
+	const showCartHanlder = () => {
+		setCartIsShow(true);
+	};
+
+	const hideCartHandler = () => {
+		setCartIsShow(false);
+	};
+
+	return (
+		<CartProvider>
+			{cartIsShow && <Cart onClose={hideCartHandler} />}
+			<Header onShowCart={showCartHanlder} />
+			<main>
+				<Meals />
+			</main>
+		</CartProvider>
+	);
+}
+
+export default App;
